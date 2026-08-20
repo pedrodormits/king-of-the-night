6f3b693c14914a51135ee9cbf30e43eb93abbd9e
PedroDormits <pdormits@gmail.com>
2026-08-20 02:41:36 +0200

    Player Code Conventions

--- Assets/Scripts/Characters/Player/PlayerAnimation.cs
+++ Assets/Scripts/Characters/Player/PlayerAnimation.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    #region COMPONENTS
+    #region Variables
     private Animator _anim;
     private Player _player;
     private Rigidbody _rb;
@@ -21,39 +21,63 @@ public class PlayerAnimation : MonoBehaviour
         PlayIdleMoveAnimations();
         PlayJumpFallAnimations();
     }
+    #region Animation State Upadetes
 
-    #region ANIMATION STATE UPDATES
-    private void UpdateIsGrounded() => _anim.SetBool("isGrounded", _player.IsGrounded);
+    private void UpdateIsGrounded()
+    {
+        _anim.SetBool("isGrounded", _player.IsGrounded);
+    } 
     
     private void PlayIdleMoveAnimations()
     {
-        Vector3 horizontalVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+        Vector3 horizontalVelocity = new Vector3(
+            _rb.linearVelocity.x,
+            0f,
+            _rb.linearVelocity.z);
+        
         _anim.SetFloat("velocityMagnitude", horizontalVelocity.magnitude);
     }
-    
-    private void PlayJumpFallAnimations() => _anim.SetFloat("verticalVelocity", _rb.linearVelocity.y);
+
+    private void PlayJumpFallAnimations()
+    {
+        _anim.SetFloat("verticalVelocity", _rb.linearVelocity.y);
+    }
     #endregion
 
-    #region COMBO INDEX
-    public void SetGroundComboIndex(int groundComboIndex) =>
+    #region Combo Index
+    public void SetGroundComboIndex(int groundComboIndex)
+    {
         _anim.SetInteger("groundComboIndex", groundComboIndex);
+    }
 
-    public void SetAirComboIndex(int airComboIndex) => _anim.SetInteger("airComboIndex", airComboIndex);
+    public void SetAirComboIndex(int airComboIndex)
+    {
+        _anim.SetInteger("airComboIndex", airComboIndex);
+    } 
     #endregion
 
-    #region ANIMATION TRIGGERS
-    public void PlayHeavyAttackAnimation() => _anim.SetTrigger(_player.CurrentPlayerAttackData.AnimTrigger);
+    #region Animation Triggers
+    public void PlayHeavyAttackAnimation()
+    {
+        _anim.SetTrigger(_player.CurrentPlayerAttackData.AnimTrigger);
+    }
 
-    public void PlayAbilityAnimation() => _anim.SetTrigger(_player.CurrentPlayerAbilityData.AnimTrigger);
+    public void PlayAbilityAnimation()
+    {
+        _anim.SetTrigger(_player.CurrentPlayerAbilityData.AnimTrigger);
+    }
 
-    public void PlayUltimateAttackAnimation() => _anim.SetTrigger("UltimateAttack");
+    public void PlayUltimateAttackAnimation()
+    {
+        _anim.SetTrigger("UltimateAttack");
+    }
 
     public void PlayHurtAnimation() => _anim.SetTrigger("takeDamage");
     
     public void PlayDeathAnimation() => _anim.SetTrigger("die");
     #endregion
     
-    #region GET ANIMATION LENGHT
+    #region Get Animation Lenght
     public float GetAnimationLength(string clipName)
     {
         foreach (var clip in _anim.runtimeAnimatorController.animationClips)
