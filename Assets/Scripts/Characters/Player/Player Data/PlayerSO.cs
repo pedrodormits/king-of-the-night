9130a2810ad5df372828f225932a01b7e8228bef
PedroDormits <pdormits@gmail.com>
2026-08-20 21:02:08 +0200

    Player SO Code Conventions

--- Assets/Scripts/Characters/Player/Player Data/PlayerSO.cs
+++ Assets/Scripts/Characters/Player/Player Data/PlayerSO.cs
@@ -2,46 +2,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Stores the player's combat and ability data.
-/// PlayerSO is a ScriptableObject, allowing attack, combo, ability, and
-/// ultimate data to be configured separately from the Player script.
+/// Stores the configuration data for a playable character.
+/// Contains the character's attacks, combo
+/// settings, abilities, and ultimate attack data.
 /// </summary>
 [CreateAssetMenu(menuName = "Characters/Player")]
 public class PlayerSO : ScriptableObject
 {
     [Header("Attack")]
-    // Contains all ground-based attacks available to the player.
     public List<PlayerAttackSO> GroundAttacks;
-
-    // Contains all air-based attacks available to the player.
     public List<PlayerAttackSO> AirAttacks;
 
     [Header("Combo")]
-    // Determines how long the player can wait between attacks while still
-    // continuing the current combo.
+    // Maximum amount of time allowed between attacks to continue a combo.
     public float ComboBufferWindow;
 
-    // Contains the animation data used for ground light attack combos.
+    // Animation data for the player's ground light attack combo.
     public ComboAttack[] GroundLightAttacks;
 
-    // Contains the animation data used for air light attack combos.
+    // Animation data for the player's air light attack combo.
     public ComboAttack[] AirLightAttacks;
 
     [Header("Abilities")]
-    // Contains all special abilities available to the player.
     public List<PlayerAbilitySO> Abilities;
 
     [Header("Ultimate")]
-    // Contains the data for the player's ultimate ability.
     public UltimateSO Ultimate;
 
     /// <summary>
-    /// Stores the animation information for an individual combo attack.
+    /// Stores the animation information required for an individual combo attack.
     /// </summary>
     [System.Serializable]
     public class ComboAttack
     {
-        // Name of the animation that should be played for this combo attack.
         public string AnimName;
     }
 }
