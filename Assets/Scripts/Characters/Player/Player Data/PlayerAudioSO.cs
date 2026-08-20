dd3d447d46f54c2c9a32a7a5e6280995c7ee0d60
PedroDormits <pdormits@gmail.com>
2026-08-20 20:52:05 +0200

    Player Audio SO Code Conventions

--- Assets/Scripts/Characters/Player/Player Data/PlayerAudioSO.cs
+++ Assets/Scripts/Characters/Player/Player Data/PlayerAudioSO.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
+/// <summary>
+/// Stores the audio clips used by the player.
+/// Contains audio for taking damage and for the ultimate ability.
+/// </summary>
 [CreateAssetMenu(menuName = "Audio/Player")]
 public class PlayerAudioSO : ScriptableObject
 {
     [Header("Hurt")]
     public AudioClip HurtAudioClip;
-    
+
     [Header("Ultimate")]
     public AudioClip UltimateChargedAudioClip;
     public AudioClip UltimateAudioClip;
