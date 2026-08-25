f9fddcbb8224aa75fa4edbc0269dc3910285d9e9
PedroDormits <pdormits@gmail.com>
2026-08-25 02:02:31 +0200

    Main Menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,7 +7,25 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _quitButton;
     [SerializeField] private string _levelToLoad;
-    
+
+    private void Start()
+    {
+        if (_playButton == null)
+        {
+            Debug.Log("Play Button is null");
+        }
+        
+        if (_quitButton == null)
+        {
+            Debug.Log("Quit Button is null");
+        }
+        
+        if (_quitButton == null)
+        {
+            Debug.Log("Level To Load is null");
+        }
+    }
+
     public void PlayGame() => SceneManager.LoadScene(_levelToLoad);
     
     public void QuitGame() => Application.Quit();
